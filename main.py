25a09581d0db675364e6a199526e27f22b697352
Githuhfc <midegaharun94@gmail.com>
2025-09-16 11:32:50 -0700

    First commit

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 from fastapi import FastAPI
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return{"message","Hello, API World"}
+contacts = {}
+
+
+@app.post("/contacts/{contact_id}")
+def create_contact(contact_id: int,name: str, phone:str):
+    contacts[contact_id] = {"name":name,"phone":phone}
+    return {"message":"Contact added","data":
+            contacts[contact_id]}
+
+
+
+@app.get("/contacts/{contact_id}")
+def read_contact(contact_id:int):
+    return contacts.get(contact_id,{"error": "Contact not found"})
+@app.put("/contacts/{contact_id}")
+def update_contact(contact_id:int,name:str,phone:str):
+    if contact_id in contacts:
+        contacts[contact_id] = {"name": name,"phone":phone}
+        return {"message":"Contact updated","data": contacts[contact_id]}
+    return {"error":"Contacts not found"}
+@app.delete("/contacts/{contact_id}")
+def delete_contact(contact_id:int):
+    if contact_id in contacts:
+        deleted = contacts.pop(contact_id)
+        return{"message":"contact deleted","data":deleted}
+    return{"error":"Contact not found" }
+
+
